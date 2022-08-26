9f766e061831c632eadb2e5e1e4279840a0672dd
John <jsaita48@gmail.com>
2022-08-26 06:06:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+
 // Add your code here
+
+function submitData(name,email){
+  const users ={
+    'name':name,
+    'email':email
+  }
+  const postHandle = {
+    method:'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(users)
+  }
+  const body =document.querySelector('body');
+  const p =document.createElement('P');
+ return fetch('http://localhost:3000/users',postHandle)
+  .then(res=>res.json())
+  .then(data=>{
+    p.innerText =data.id
+    document.body.appendChild(p)
+  })
+  .catch((message)=>{
+    alert(message)
+    p.innerText ="Unauthorized Access"
+    document.body.appendChild(p)
+
+  })
+
+}
+
+
+
